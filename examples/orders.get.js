6e380c6ea55d8db4c67e5c0e3c2a51fbb02f761b
pitkane <mikko@mbit.fi>
2016-10-27 18:39:35 +0200

    :neckbeard:

--- examples/orders.get.js
+++ examples/orders.get.js
@@ -8,19 +8,19 @@ epages.configure({
   'shop_name': 'DemoShop',
   'token': 'oddsVYIR0BY1sGTZoFmLthZzA66kykhS',
 });
-console.log(epages);
 
-// console.log(epages)
+console.log(epages)
 
-console.log(epages.shop)
-
-// epages.shop.get()
-//     .then( function(data) {
-//         console.log("Got Shop");
-//         // console.log("Error: " + error);
-//         console.log("Response: " + data);
-//         // console.log(JSON.stringify(data));
-//     })
+epages.shop.get('asdf')
+    .then( function(data) {
+        console.log("Got Shop");
+        // console.log("Error: " + error);
+        console.log("Response: " + data);
+        // console.log(JSON.stringify(data));
+    })
+    .catch( function(shi) {
+        console.log(shi);
+    });
 //
 // function wait () {
 //    if (true) {
